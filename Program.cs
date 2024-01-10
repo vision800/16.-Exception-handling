21d4efd6923272a780e097d84806e64312fd1a2c
vision800 <netshokotshokovision112@gmail.com>
2024-01-10 10:10:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.Exception_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                Console.WriteLine("Enter a number");
+                int num1= int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter another number");
+                int num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine(num1/num2);
+            }
+            catch (Exception e){ 
+            Console.WriteLine(e.Message);
+            }
+            Console.Read();
+        }
+    }
+}
